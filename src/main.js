1b4717838901220ff415002dd3097c9f1f1ffc1d
joeyanovich <joedison.dias@gmail.com>
2022-10-19 22:48:25 -0300

    :heavy_plus_sign: Add: images (cards logo 'svg')

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
 function setCardType(type) {
   const colors = {
-    visa: ["#436D99", "#2D57F2"],
-    mastercard: ["#DF6F29", "#C69347"],
+    visa: ["#2D57F2", "#F3E989"],
+    mastercard: ["#A5914B", "#D4424B"],
     default: ["black", "gray"],
   }
 
