5b0a5e01addada0195237ca34b9cce0d1582a1ee
joeyanovich <joedison.dias@gmail.com>
2022-10-23 16:45:18 -0300

    :hammer: Feat: removed form reload when click submit button

--- src/main.js
+++ src/main.js
@@ -97,3 +97,12 @@ const cardNumberPattern = {
 }
 
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+const addButton = document.querySelector("#add-card")
+addButton.addEventListener("click", () => {
+  console.log("Botão clicado")
+})
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault()
+})
